f32e8392c28293cbe6690e252557dcbb9a34d7ce
蓝澈 <cleanblue@lanchedeAir.lan>
2021-09-21 16:03:55 +0800

    模版：字符串哈希

--- templates/vector_hash.cc
+++ templates/vector_hash.cc
@@ -4,7 +4,7 @@ using namespace std;
 template <class _data_class = int, int _base = 13331> constexpr uint64_t vector_hash(const vector<_data_class> &vec) {
     uint64_t res = 0;
     for (const auto element : vec) {
-        res = _base * (uint64_t(element) + res + _base);
+        res = _base * res + element + _base;
     }
     return res;
 }
