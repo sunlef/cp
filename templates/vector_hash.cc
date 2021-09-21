2b36a8e14d346c514d27592e346646332ed77c61
蓝澈 <cleanblue@lanchedeAir.lan>
2021-09-21 13:56:46 +0800

    模版：vector哈希

---
+++ templates/vector_hash.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class _data_class = int, int _base = 13331> constexpr uint64_t vector_hash(const vector<_data_class> &vec) {
+    uint64_t res = 0;
+    for (const auto element : vec) {
+        res = _base * (uint64_t(element) + res + _base);
+    }
+    return res;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (auto &x : vec) {
+        cin >> x;
+    }
+    cout << vector_hash<int, 2233>(vec) << '\n';
+
+    return 0;
+}
